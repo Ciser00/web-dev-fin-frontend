f9f811eadd0952cf7e6b54628f918ce6404b0c91
Ciser00 <47250680+Ciser00@users.noreply.github.com>
2021-12-13 21:52:43 -0500

    added  to user profile

---
+++ src/pages/ViewProfile.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function UserProfile({ userInformation}){
+  return (
+    <div className="PageWrapper">
+      <h1>User Profile</h1>
+      <p> Email: {userInformation.email}</p>
+      <p> Name: {userInformation.displayName}</p>
+      <p> UID: {userInformation.uid}</p>
+    </div>
+);
+
+}
+
+export default UserProfile;
